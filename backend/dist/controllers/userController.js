79e4f3138dcc99f2bfa245ab59bea57bb650763c
Nanu25 <alexandrugrancea25@gmail.com>
2025-05-15 09:02:22 +0300

    a

--- backend/dist/controllers/userController.js
+++ backend/dist/controllers/userController.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUser = exports.updateUserMetrics = exports.getUserMetrics = void 0;
+exports.deleteUser = exports.getUser = exports.updateUserMetrics = exports.getUserMetrics = void 0;
 const database_1 = require("../config/database");
 const User_1 = require("../entities/User");
+const ActivityLog_1 = require("../entities/ActivityLog");
 const getUserMetrics = async (req, res) => {
     var _a;
     try {
@@ -103,4 +104,34 @@ const getUser = async (req, res) => {
     }
 };
 exports.getUser = getUser;
+const deleteUser = async (req, res) => {
+    var _a;
+    try {
+        const { userId } = req.params;
+        if (!((_a = req.user) === null || _a === void 0 ? void 0 : _a.id)) {
+            res.status(401).json({ message: 'User not authenticated' });
+            return;
+        }
+        const userRepository = database_1.AppDataSource.getRepository(User_1.User);
+        const activityLogRepository = database_1.AppDataSource.getRepository(ActivityLog_1.ActivityLog);
+        const requestingUser = await userRepository.findOne({ where: { id: req.user.id } });
+        if (!(requestingUser === null || requestingUser === void 0 ? void 0 : requestingUser.isAdmin)) {
+            res.status(403).json({ message: 'Only admins can delete users' });
+            return;
+        }
+        const userToDelete = await userRepository.findOne({ where: { id: userId } });
+        if (!userToDelete) {
+            res.status(404).json({ message: 'User not found' });
+            return;
+        }
+        await activityLogRepository.delete({ userId: userId });
+        await userRepository.remove(userToDelete);
+        res.status(200).json({ message: 'User deleted successfully' });
+    }
+    catch (error) {
+        console.error('Error deleting user:', error);
+        res.status(500).json({ message: 'Error deleting user' });
+    }
+};
+exports.deleteUser = deleteUser;
 //# sourceMappingURL=userController.js.map
