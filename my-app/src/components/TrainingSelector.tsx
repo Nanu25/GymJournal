bc7f2110746400c6dc4117cb2ed8c14c98012a0d
Nanu25 <alexandrugrancea25@gmail.com>
2025-05-17 23:27:55 +0300

    a

--- my-app/src/components/TrainingSelector.tsx
+++ my-app/src/components/TrainingSelector.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import DividerLine from "./DividerLine";
-import MetricInput from "./MetricInput";
+
 
 interface TrainingEntry {
     date: string;
